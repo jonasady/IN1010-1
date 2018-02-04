c839e69eaaf04ac94a501c36ac63e3c8df13a74c
eirinko <eirinkorvald.ek@gmail.com>
2018-02-04 12:28:24 +0100

    New trix-assignements.

---
+++ Trix/Personinfo.java
@@ -0,0 +1,7 @@
+class Personinfo{
+  public static void main(String[] args){
+    System.out.println("Eirin Korvald");
+    System.out.println("47332999");
+    System.out.println("Skedsmogata 15B");
+  }
+}
