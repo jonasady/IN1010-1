5caaf2c11c88b723b9fd768a708a1e65b20b8bc0
eirinko <eirinkorvald.ek@gmail.com>
2018-03-10 23:50:21 +0100

    Started on oblig3, had to watch some of the lectures first.

--- oblig3/Lenkeliste.java
+++ oblig3/Lenkeliste.java
@@ -1,21 +1,57 @@
 class Lenkeliste<T> implements Liste<T>{
-  private Node start;
+  private class Node{
+    Node neste = null;
+    T data;
 
-  public class Node{
-    T value;
-    Node next;
+    Node(T x){ /*Nodens konstruktoer*/
+      T data = x;
+    }
+  }
+  private Node start = null;
 
-    public Node(T x){
-      this.value = x;
+  @Override
+  public int stoerrelse(){
+    Node p = start;
+    int n = 0;
+    while (p != null){
+      n++;
+      p = p.neste;
+    }
+    return n;
+  } /*Kan lage en egen variabel for stoerrelse.*/
+
+
+  @Override /*Foerst inn, foerst ut = queue.*/
+  public void leggTil(T x){
+    if (start == null){ /*Sjekker om lista er tom.*/
+      start.neste = new Node(x);
+    }
+    else{
+      Node p = start;
+      while (p.neste != null){ /*Finner siste sted i lista.*/
+        p = p.neste;
+      }
+      p.neste = new Node(x);
     }
   }
 
   @Override
-  public void leggTil(T x){
-    Node nyNode = new Node(x);
-    nyNode.next = Node start;
-    Node start = nyNode;
+  public T hent(int pos){
+    Node p = start;
+    for (int i = 0; i < pos; i++){
+      Node p = p.neste;
+    }
+    return
   }
 
-  
+
+  @Override
+  public T fjern(int pos){
+
+  }
+
+  @Override
+  public T fjern(){
+
+  }
 }
